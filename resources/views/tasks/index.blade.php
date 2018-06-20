bea234a3df8ed403d1483ca74ae08a7c404a7e1e
Ricardo <richardaccounts@hotmail.com>
2018-06-21 00:10:08 +0100

    shit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <ul>
+        @foreach ($tasks as $task)
+            <a href="/tasks/{{$task->id}}">
+                <li>{{$task->body}}</li>
+            </a>
+        @endforeach
+
+    </ul>
+</body>
+</html>
