498c18911f8f7e76ea50936dc441540eb28145c2
Ricardo Silva <ricardowork95@gmail.com>
2018-09-12 18:45:29 +0100

    23

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
 
 class ExampleTest extends TestCase
-{
+{   use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -14,8 +16,26 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+       $first = factory(Post::class)->create();
+
+       $second = factory(Post::class)->create([
+           'created_at' => \Carbon\Carbon::now()->subMonth()
+       ]);
+
+       $posts = Post::archives();
+
+       $this->assertEquals([
+           [
+               "year" => $first->created_at->format('Y'),
+               "month" => $first->created_at->format('F'),
+               "published" => 1
+           ],
+           [
+               "year" => $second->created_at->format('Y'),
+               "month" => $second->created_at->format('F'),
+               "published" => 1
+           ]
+       ], $posts);
     }
 }
